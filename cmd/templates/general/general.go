78fd49344548ec46ca7961a6615d370b60a1ea18
0xshen <themhsalimi@gmail.com>
2024-09-26 09:27:36 +0330

    taskfile template

--- cmd/templates/general/general.go
+++ cmd/templates/general/general.go
@@ -2,7 +2,7 @@ package general
 
 import _ "embed"
 
-var GeneralFiles = map[string][]byte{"README.md": ReadMeTemplate(), ".gitignore": GitIgnoreTemplate()}
+var GeneralFiles = map[string][]byte{"README.md": ReadMeTemplate(), ".gitignore": GitIgnoreTemplate(), "Taskfile.yml": TaskFileTemplate()}
 
 //go:embed files/README.md.tmpl
 var readMeTemplate []byte
@@ -10,10 +10,17 @@ var readMeTemplate []byte
 //go:embed files/.gitignore.tmpl
 var gitIgnoreTemplate []byte
 
+//go:embed files/Taskfile.yml.tmpl
+var taskFileTemplate []byte
+
 func ReadMeTemplate() []byte {
 	return readMeTemplate
 }
 
 func GitIgnoreTemplate() []byte {
 	return gitIgnoreTemplate
+}
+
+func TaskFileTemplate() []byte {
+	return taskFileTemplate
 }
